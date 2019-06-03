d7718935a3cfda4b1e50fe248319b31f81bd9ad4
Nixon <nixnmtm@users.noreply.github.com>
2019-06-03 18:10:25 +0800

    minor changes

--- MGTools.py
+++ MGTools.py
@@ -1,18 +1,55 @@
 import pandas as pd
 import numpy as np
 import MDAnalysis as mda
+import logging
+import ProTools
 
+pt = ProTools
 
 class MGTools(object):
 
-    def __init__(self):
+    def __init__(self, table, ressep):
 
         self.grouping = ["segidI", "resI", "segidJ", "resJ"]
         self._index = ["segidI", "resI", "I", "segidJ", "resJ", "J"]
+        self.table = table
+        self.ressep = ressep
 
-    def init_table(self, filepath, filename, ressep):
-        table = filepath/filename
-        return table
+    def split_sec_struc(self, psf=None, write=False, prefix=None):
+        # split table into three tables based on BB,BS and SS
+        tmp = self.table.copy(deep=True)
+        if psf:
+            # if atom names are not defined in the table
+            cg = mda.Universe(psf)
+            atomnames = cg.atoms.names()
+            atomids = np.unique(self.table["I"])
+            atmname_atmid = dict(list(zip(atomids, atomnames)))
+            tmp['I'].replace(atmname_atmid, inplace=True)
+            tmp['J'].replace(atmname_atmid, inplace=True)
+
+        # BACKBONE-BACKBONE
+        BB = tmp[((tmp["I"] == 'N') | (tmp["I"] == 'O') | (tmp["I"] == 'ions')) \
+                  & ((tmp["J"] == 'N') | (tmp["J"] == 'O') | (tmp["J"] == 'ions'))]
+
+        # BACKBONE-SIDECHAIN
+        BS = tmp[((tmp["I"] == "N") | (tmp["I"] == 'O') | (tmp["I"] == "ions")) & (tmp["J"] == 'CB')]
+        SB = tmp[(tmp["I"] == 'CB') & ((tmp["J"] == "N") | (tmp["J"] == 'O') | (tmp["J"] == "ions"))]
+        BB_side = pd.concat([BS, SB], axis=0, ignore_index=True)
+
+        # SIDECHAIN-SIDECHAIN
+        SS = tmp[(tmp["I"] == "CB") & (tmp["J"] == "CB")]
+
+        if write:
+            if prefix is None:
+                logging.error("prefix is not defined")
+                exit(1)
+            else:
+                # write the files in current directory
+                BB.to_csv(prefix + "_" + "kb_BB.txt", header=True, sep=" ")
+                BB_side.to_csv(prefix + "_" + "kb_BS.txt", header=True, sep=" ")
+                SS.to_csv(prefix + "_" + "kb_SS.txt", header=True, sep=" ")
+
+        return BB, BB_side, SS
 
     def sepres(self):
         tmp = self.table[self.table["segidI"] == self.table["segidJ"]]
@@ -22,23 +59,21 @@ class MGTools(object):
             ]
         diff = self.table[self.table["segidI"] != self.table["segidJ"]]
         df = pd.concat([tmp, diff], axis=0)
-        # df.set_index(self._index, inplace=True)
         return df
 
     def sum_mean(self, segid=None):
+        """Returns the sum, mean and standard deviation of residues based on the self.grouping"""
         tab_sep = self.sepres()
         if segid:
             tab_sep = tab_sep[(tab_sep["segidI"] == segid) & (tab_sep["segidJ"] == segid)]
         tab_sum = tab_sep.groupby(self.grouping).sum()
         tab_mean = tab_sum.mean(axis=1)
-        tab_var = tab_sum.var(axis=1)
         tab_std = tab_sum.std(axis=1)
-        return tab_sum, tab_mean, tab_std, tab_var
+        return tab_sum, tab_mean, tab_std
 
-    def csm_mat(self):
+    def csm_mat(self, tab):
         """Returns symmetric diagonally dominant residue-residue coupling strength matrix (CSM)"""
 
-        _, tab, _, _ = self.sum_mean()
         try:
             tab.ndim == 1
         except TypeError:
@@ -71,13 +106,15 @@ class MGTools(object):
                 eigenVectors[:, k] = np.sign(np.mean(eigenVectors[:, k])) * eigenVectors[:, k]
         return eigenValues, eigenVectors
 
-    def t_eigenVect(self, sum_mat, Nwind, start, end, Npos, SS=False):
+    def t_eigenVect(self, Nwind, start, end, Npos, SS=False):
         """ Return csm_mat, eigenVectors and eigenValues of all windows """
+
+        stab, _, _ = self.sum_mean()
         t_mat = np.zeros((Nwind, Npos, Npos))
         t_vec = np.zeros((Nwind, Npos, Npos))
         t_val = np.zeros((Nwind, Npos))
         for i in range(Nwind):
-            time_mat = self.csm_mat(sum_mat.iloc[:, i])
+            time_mat = self.csm_mat(stab.iloc[:, i])
             if SS:
                 time_mat = time_mat.reindex(np.arange(start, end)).T.reindex(np.arange(start, end)).replace(np.nan, 0.0)
             tval, tvec = self.eigenVect(time_mat)
@@ -99,51 +136,17 @@ class MGTools(object):
                 max1 = sorted(np.square(pers_mat[w][m]))[-1]
                 pers = max1
                 leak = 1 - max1
-                # if sorted(np.square(pers_mat[w][m]))[-2] > 0.4:
-                #    max2 = sorted(np.square(pers_mat[w][m]))[-2]
-                #    pers = max1 + max2
-                #   leak = 1-(max1 + max2)
                 lk.append(leak)
                 ps.append(pers)
             mleak.append(np.asarray(lk).mean())
             mpers.append(np.asarray(ps).mean())
         return mpers, mleak
 
-    def split_sec_struc(self, psf, prefix, write=False):
-        # split table into three tables based on BB,BS and SS
-        cg = mda.Universe(psf)
-        atomnames = cg.atoms.names()
-        atomids = np.unique(self.table["I"])
-        atmname_atmid = dict(list(zip(atomids, atomnames)))
-        tmp = self.table.copy(deep=True)
-        tmp['I'].replace(atmname_atmid, inplace=True)
-        tmp['J'].replace(atmname_atmid, inplace=True)
-        # BACKBONE-BACKBONE
-        B_B = tmp[((tmp["I"] == 'N') | (tmp["I"] == 'O') | (tmp["I"] == 'ions')) \
-                  & ((tmp["J"] == 'N') | (tmp["J"] == 'O') | (tmp["J"] == 'ions'))]
-
-        # BACKBONE-SIDECHAIN
-        B_S = tmp[((tmp["I"] == "N") | (tmp["I"] == 'O') | (tmp["I"] == "ions")) & (tmp["J"] == 'CB')]
-        S_B = tmp[(tmp["I"] == 'CB') & ((tmp["J"] == "N") | (tmp["J"] == 'O') | (tmp["J"] == "ions"))]
-        BB_side = pd.concat([B_S, S_B], axis=0, ignore_index=True)
-
-        # SIDECHAIN-SIDECHAIN
-        S_S = tmp[(tmp["I"] == "CB") & (tmp["J"] == "CB")]
-
-        if write:
-            # write the files in current directory
-            B_B.to_csv(prefix + "_" + "kb_BB.txt", header=True, sep=" ")
-            BB_side.to_csv(prefix + "_" + "kb_BS.txt", header=True, sep=" ")
-            S_S.to_csv(prefix + "_" + "kb_SS.txt", header=True, sep=" ")
-
-        return B_B, BB_side, S_S
-
     @staticmethod
     def significant_residues(eigenVector, modes, cutoff):
         # Get sigificant residues from significant modes
         r = list()
         for j in modes:
-            # for i in np.where(eigenVector[:,j-1] >= cutoff): #signifcnt residues only from +ive residues, not used anymore
             for i in np.where(eigenVector[:, j - 1] * eigenVector[:, j - 1] >= cutoff):
                 r.append(i + 1)
         top_res = np.sort(np.unique(np.concatenate(r, axis=0)))
@@ -184,6 +187,7 @@ class MGTools(object):
         pres = list()
         nres = list()
         sres = list()
+
         if pdb:
             for m, n, s in zip(self.pdbno, U[:, mode - 1], (U[:, mode - 1] * U[:, mode - 1])):
                 if s > wcut:
