dde16a1949cdc7d4971ec13d440ae419619bc8cc
Nixon <nixnmtm@users.noreply.github.com>
2019-06-05 16:05:16 +0800

    June 5 2019 1. ProTools superclass for subclass MGTools 2. Added logging 3. start and end retrieved from table itself

--- test.py
+++ test.py
@@ -1,25 +1,16 @@
-from ProTools import ProTools
 from MGTools import MGTools
-prot = ProTools("1BE9")
-# print(prot.univ.segments[1].segid)
-# print(prot.get_residNname(prot.univ.segments[0].segid)[1][0])
-# print(prot.get_residNname(prot.univ.segments[0].segid)[1][-1])
-u = prot.univ
 import pandas as pd
 import numpy as np
-#dd = pd.read_csv("C:/Users/nixon/Google Drive/coding/nw_notebooks/kb_5ms_apo_local_copy.txt", sep=" ")
-dd = pd.read_csv("C:/Users/nixon/Google Drive/coding/nw_notebooks/kb_BB.txt", sep=" ", index_col=0)
-# print(dd.head())
-# atomids = np.unique(dd["I"])
-# atmnames = u.atoms.names
-# atmname_atmid = dict(zip(atomids, atmnames))
-# dd['I'].replace(atmname_atmid, inplace=True)
-# dd['J'].replace(atmname_atmid, inplace=True)
-# print(dd.head())
 
-mgt = MGTools(dd, ressep=3)
+pdz3 = "C:/Users/nixon/Google Drive/coding/nw_notebooks/pdz3_kb_complete_apo_local_copy.txt"
+#protease = "C:/Users/nixon/Google Drive/coding/nw_notebooks/kb_5ms_apo_local_copy.txt"
+"""The table should be a complete table with residue name and atom name already included"""
+mgt = MGTools(pdbid="1BE9", table_path=pdz3, ressep=1)
 # bb, bs, ss = mgt.split_sec_struc()
-S, M, D = mgt.sum_mean()
-print(S.columns.size)
-print((S.index.get_level_values('resI').unique().size))
-# print(bb.head())
+s, m, sd = mgt.sum_mean()
+#tmat, tvec, tval = mgt.t_eigenVect(SS=True)
+#print(tval[0])
+# print(len(mgt.get_residNname(segid=mgt.univ.segments[0].segid)[1]))
+# print(len(np.unique(mgt.table.resI)))
+# print((mgt.univ.segments[0].residues.resnames == "GLY").sum())
+# print(mgt.univ.segments[0].residues.resnames[:10])
