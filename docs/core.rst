c1d574d874260948e746efd8d64394a72333936d
nixnmtm <nixnmtm@gmail.com>
2019-10-29 14:34:02 +0800

    core completed for inclusion of intersegs
    1. Compare resids of interSegs and rename if needed
    2. Creation of MGT mat with intersegs
    3. Documentation updated

--- docs/core.rst
+++ docs/core.rst
@@ -4,6 +4,9 @@ Core Modules of MGT
 :mod:`core` module consists of the base :class:`core.BuildMG` class that builds the molecular graph from the fluctuogram
 data.
 
+To analyze inter-segments, use the ``interSegs`` argument with tuple of two segments. If the residue ids in two segments
+overlap, the residue ids are renamed using :func:`_refactor_resid` for correct MG matrix creation.
+
 .. automodule:: core
     :members:
     :undoc-members:
--- src/core.py
+++ src/core.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import logging
 import numpy as np
+import string
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,39 +11,37 @@ class BuildMG(object):
     """
     Base class for building Coupling strength dataframe.
 
+    :param filename: Name of the file to be loaded
+    :param ressep: residue separation( >= I,I + ressep), (default=3)
+    :key interSegs: two segments names for inter segment analysis, should be a tuple
+    :key input_path: path of file input, should be a str, if not given searches filename in Inputs folder
+    :returns: MGT Matrix
+
+
     :Example:
     .. highlight:: python
     .. code-block:: python
 
         >>> from src.core import BuildMG
         >>> mgt = BuildMG(filename="holo_pdz.txt.bz2", ressep=1)
-        >>> print(mgt.mgt_mat())
+        >>> print(mgt.mgt_mat(seg="CRPT", sptkey="BB"))
 
-                  5         6         7         8         9
-        5  0.025300  0.022754  0.002543  0.000003  0.000000
-        6  0.022754  0.105062  0.068219  0.013300  0.000788
-        7  0.002543  0.068219  0.239772  0.086187  0.082822
-        8  0.000003  0.013300  0.086187  0.370769  0.271280
-        9  0.000000  0.000788  0.082822  0.271280  0.354890
+                    5         6         8         9
+            5  0.146590  0.000000  0.139217  0.007373
+            6  0.000000  0.052921  0.000000  0.052921
+            8  0.139217  0.000000  0.139217  0.000000
+            9  0.007373  0.052921  0.000000  0.060294
 
     """
 
     def __init__(self, filename: str, ressep=3, **kwargs):
-        """
-        :func:`__init__` method docstring.
-        Creates a new :class:`BuildMG` instance.
 
-        :param filename: Name of the file to be loaded
-        :param ressep: residue separation( >= I,I + ressep), (default=3)
-        :type int
-        :param splitMgt: split mgt into BB or BS or SS
-        :type str
-        :param segid: segment id to analyze, if not mentioned all segid is used
-        :type str
-        """
         self.filename = filename
         self.module_path = os.path.dirname(os.path.realpath(__file__))
-        self.input_path = os.path.join(self.module_path + "/../Inputs")
+        if kwargs.get("input_path") is None:
+            self.input_path = os.path.join(self.module_path + "/../Inputs")
+        else:
+            self.input_path = kwargs.get("input_path")
         self.table = self.load_table()
         self.grouping = ["segidI", "resI", "segidJ", "resJ"]
         self._index = ["segidI", "resI", "I", "segidJ", "resJ", "J"]
@@ -63,24 +62,30 @@ class BuildMG(object):
         if not (fileext[-3:] == "txt" or fileext[-3:] == "bz2"):
             logging.error("Please provide a appropriate file, with extension either txt or bz2")
         filepath = os.path.join(self.input_path, self.filename)
-        os.path.exists(filepath)
-        table = pd.read_csv(os.path.join(filepath), sep=' ')
-        if str(table.columns[0]).startswith("Unnamed"):
-            table = table.drop(table.columns[0], axis=1)
-        logging.info("File loaded.")
-        return table
+        try:
+            table = pd.read_csv(filepath, sep=' ')
+            if str(table.columns[0]).startswith("Unnamed"):
+                table = table.drop(table.columns[0], axis=1)
+            logging.info("File loaded.")
+            return table
+        except IOError as e:
+            logging.error(f'Error in loading file: {str(e)}')
 
     def splitSS(self, write: bool = False) -> dict:
         """
         Split based on secondary structures.
 
-        BB - Backbone-Backbone Interactions
-        BS - Backbone-Sidechain Interactions
-        SS - Sidechain-Sidehain Interactions
+        | BB - Backbone-Backbone Interactions
+        | BS - Backbone-Sidechain Interactions
+        | SS - Sidechain-Sidehain Interactions
 
         :param df: Dataframe to split. If None, df initialized during class instance is taken
         :param write: write after splitting
         :return: dict of split DataFrames
+
+        .. todo::
+            1. Try to include ion interaction with SS
+            2. Remove ion interactions from BS
 
         """
         # split table into three tables based on BB,BS and SS
@@ -90,7 +95,6 @@ class BuildMG(object):
 
         sstable = dict()
         tmp = self.table.copy(deep=True)
-
         try:
             # BACKBONE-BACKBONE
             sstable['BB'] = tmp[((tmp["I"] == 'N') | (tmp["I"] == 'O') | (tmp["I"] == 'ions')) \
@@ -118,6 +122,8 @@ class BuildMG(object):
 
     def sepres(self, table) -> object:
         """
+        Residue Separation
+
         :param table: table for sequence separation
         :param ressep: sequence separation to include (eg.  >= I,I + ressep), default is I,I+3)
         :return: DataFrame after separation
@@ -138,32 +144,28 @@ class BuildMG(object):
         """
         Returns the sum table based on the self.grouping
 
-        :return: dict of sunm tables
+        :return: dict of sum tables
 
         :Example:
         .. highlight:: python
         .. code-block:: python
 
-        >>> from src.core import BuildMG
-        >>> mgt = BuildMG(filename="holo_pdz.txt.bz2", ressep=3, interSegs=("PDZ3", "CRPT"))
-        >>> print(mgt.table_sum())
-
-        {"CRPT": {"BB" :  df},
-                 {"BS" :  df},
-                 {"SS" :  df}
-        ,
-        "PDZ3": {"BB" :  df},
-                {"BS" :  df},
-                {"SS" :  df}
-        },
-        ("PDZ3", "CRPT"): {"BB" :  df},
-                          {"BS" :  df},
-                          {"SS" :  df}
-        },
+         >>> from src.core import BuildMG
+         >>> mgt = BuildMG(filename="holo_pdz.txt.bz2", ressep=3, interSegs=("PDZ3", "CRPT"))
+         >>> print(mgt.table_sum())
+         {
+         "CRPT":           {"BB": df, "BS": df, "SS": df},
+         "PDZ3":           {"BB": df, "BS": df, "SS": df},
+         ("PDZ3", "CRPT"): {"BB": df, "BS": df, "SS": df}
+         }
 
         """
+
         smtable = dict()
         sstable = self.splitSS()
+        if self.interSegs is not None:
+            seg1 = self.interSegs[0]
+            seg2 = self.interSegs[1]
 
         for seg in self.table.segidI.unique():
             smtable[seg] = dict()
@@ -175,17 +177,22 @@ class BuildMG(object):
 
         if self.interSegs is not None and isinstance(self.interSegs, tuple):
             smtable[self.interSegs] = dict()
-            if self.interSegs[0] == self.interSegs[1]:
+            if seg1 == seg2:
                 raise IOError("Inter segments should not be same")
             for key in self.splitkeys:
                 tmp = self.sepres(table=sstable[key]).groupby(self.grouping).sum()
-                mask = (tmp.index.get_level_values("segidI") == self.interSegs[0]) & \
-                       (tmp.index.get_level_values("segidJ") == self.interSegs[1])
-                revmask = (tmp.index.get_level_values("segidI") == self.interSegs[1]) & \
-                       (tmp.index.get_level_values("segidJ") == self.interSegs[0])
-                intertab = pd.concat([tmp[mask], tmp[revmask]], axis=0)
-                fullsm = pd.concat([smtable[self.interSegs[0]][key], smtable[self.interSegs[1]][key]], axis=0)
-                smtable[self.interSegs][key] = pd.concat([fullsm, intertab], axis=0)
+                mask = (tmp.index.get_level_values("segidI") == seg1) & \
+                       (tmp.index.get_level_values("segidJ") == seg2)
+                revmask = (tmp.index.get_level_values("segidI") == seg2) & \
+                       (tmp.index.get_level_values("segidJ") == seg1)
+                diff = pd.concat([tmp[mask], tmp[revmask]], axis=0)
+                same = pd.concat([smtable[seg1][key], smtable[seg2][key]], axis=0)
+                inter = pd.concat([same, diff], axis=0)
+                if self._comp_resid(inter):
+                    logging.warning("resids overlap, refactoring resids")
+                    inter = self._refactor_resid(inter)
+                smtable[self.interSegs][key] = inter
+
 
         return smtable
 
@@ -196,60 +203,92 @@ class BuildMG(object):
         :return: dict of mean tables, format as table_sum()
 
         """
-
         table = self.table_sum()
         mntable = dict()
         for seg in table.keys():
             mntable[seg] = {key: table[seg][key].mean(axis=1) for key in table[seg].keys()}
         return mntable
 
-    def mgt_mat(self, segid=None, ressep=None):
+    def _nres(self, segid=None, ss=None):
         """
-        Build MGT Matrix
+        Return number of residues in a segmet or inter-segments
 
-        :param segid: input segment id
-        :param ressep: input residue separation
-        :param segsplit: True: split matrix based on segids(ie. if 2 segids present, list of 2 dataframes are returned)
-                         False: A single matrix with inter- & intra- segement interactions is returned
-        :return: MGT dataframe
+        :param segid: segment id
+        :param ss: splitkey
+        :return: number of residue (int)
 
         """
+        return len(self.stab[segid][ss].index.get_level_values("resI").unique())
 
+    def _resids(self, segid=None, ss=None):
+        """
+        Return resids of given segments.
 
-        def mgtcore(df):
-            """
+        :param seg: segid
+        :return: array of residue ids
+        """
 
-            :param df:
-            :return:
-            """
-            #tmp = df.copy(deep=True)
-            diag_val = df.groupby("resI").sum().drop("resJ", axis=1).values.ravel()
-            ref_mat = df.drop(["segidI", "segidJ"], axis=1).set_index(['resI', 'resJ']).unstack(fill_value=0).values
-            row, col = np.diag_indices(ref_mat.shape[0])
-            ref_mat[row, col] = diag_val
-            ref_mat = pd.DataFrame(ref_mat, index=np.unique(df.resI.values), columns=np.unique(df.resI.values))
-            # # Maintain table shape by adding zero for glycine residues missing in SS
-            # if self.splitMgt == "SS":
-            #     start = np.unique(tmp["resI"].values)[0]  # start residue number
-            #     end = np.unique(tmp["resI"].values)[-1]  # end residue number
-            #     ref_mat = ref_mat.reindex(np.arange(start, end+1)).T.reindex(np.arange(start, end+1)).replace(np.nan, 0.0)
-            return ref_mat
+        return self.stab[segid][ss].index.get_level_values("resI").unique()
 
-        _, tab, _ = self.sum_mean(segid=segid, ressep=ressep)
+    def _refactor_resid(self, df):
+        """
+        Rename the resids of inter segments.
+        | Method should be called only if segments has overlapping resids
 
-        try:
-            tab.ndim == 1
-        except TypeError:
-            logging.error('Dimension of the mat should not exceed 1, as we are stacking from each column')
+        :param df: Dataframe with overalpping resids in segments
+        :return: DataFrame with renamed resids
+        """
+        alphs = list(string.ascii_uppercase)
+        if self.interSegs is not None:
+            if isinstance(df.index, pd.core.index.MultiIndex):
+                df = df.reset_index()
+            for n, seg in enumerate(self.interSegs):
+                resids = df[df.segidI == seg].resI.unique()
+                if not resids.dtype == str:
+                    mapseg = [alphs[n] + str(i) for i in resids]
+                    mapd = dict(zip(resids, mapseg))
+                    df.loc[df['segidI'] == seg, 'resI'] = df['resI'].map(mapd)
+                    df.loc[df['segidJ'] == seg, 'resJ'] = df['resJ'].map(mapd)
+            renamed = df.set_index(self.grouping)
         else:
-            _tab = tab.reset_index()
-            segments = np.unique(_tab["segidI"].values)
-            print(segments)
-            if len(segments) > 1:
-                dfs = list()
-                for seg in segments:
-                    tmp = _tab[(_tab["segidI"] == seg) & (_tab["segidJ"] == seg)]
-                    dfs.append(mgtcore(tmp))
-                return dfs
-            else:
-                return mgtcore(_tab)
+            logging.warning(f"interSegs argumet is None, but {self._refactor_resid.__name__} invoked")
+            pass
+        return renamed
+
+    def _comp_resid(self, df):
+        """
+        Compare resids of the two segments and return True if overlap exists in resids
+
+        :param df: DataFrame to check for comparision
+        :return: Boolean
+        """
+
+        if self.interSegs is not None:
+            if isinstance(df.index, pd.core.index.MultiIndex):
+                df = df.reset_index()
+            r1 = df[df.segidI == self.interSegs[0]].resI.unique()
+            r2 = df[df.segidI == self.interSegs[1]].resI.unique()
+            return np.intersect1d(r1, r2).size > 0
+
+    def mgt_mat(self, seg=None, sptkey=None):
+        """
+        Build MGT Matrix.
+        | Input should be a :class:`pd.Series`
+
+        :param df: Mean series or window series
+        :param seg: segment to convert, inter-segmets can be given as tuple --> eg:("PDZ3", "CRPT")
+        :param sptkey: splitkey of segment to convert (eg: BB)
+        :return:  MGT matrix, type dataframe
+        """
+
+        tab = self.table_mean()[seg][sptkey]
+        if isinstance(tab, pd.Series) and isinstance(tab.index, pd.core.index.MultiIndex):
+            tab = tab.reset_index()
+        if tab.groupby("resI").sum().shape[0] > 1:
+            diag_val = tab.groupby("resI").sum().drop("resJ", axis=1).values.ravel()
+        else:
+            diag_val = tab.groupby("resI").sum().values.ravel()
+        ref_mat = tab.drop(["segidI", "segidJ"], axis=1).set_index(['resI', 'resJ']).unstack(fill_value=0).values
+        row, col = np.diag_indices(ref_mat.shape[0])
+        ref_mat[row, col] = diag_val
+        return pd.DataFrame(ref_mat, index=np.unique(tab.resI.values), columns=np.unique(tab.resI.values))
